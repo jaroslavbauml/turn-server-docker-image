215ba841d06d097be6345aec1e904398b5fb148d
jaroslavbauml <jaroslavbauml@gmail.com>
2018-11-13 10:56:27 +0100

    Update deploy-turnserver.sh

--- deploy-turnserver.sh
+++ deploy-turnserver.sh
@@ -1,6 +1,6 @@
-echo $1
-echo $2
-echo $3
+echo $usr
+echo $psw
+echo $realm
 
 internalIp="$(ip a | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
 externalIp="$(dig +short myip.opendns.com @resolver1.opendns.com)"
@@ -10,8 +10,8 @@ tls-listening-port=5349
 listening-ip="$internalIp"
 relay-ip="$internalIp"
 external-ip="$externalIp"
-realm=$3
-server-name=$3
+realm=$realm
+server-name=$realm
 lt-cred-mech
 userdb=/var/lib/turn/turndb
 # use real-valid certificate/privatekey files
@@ -21,8 +21,8 @@ pkey=/etc/ssl/turn_server_pkey.pem
 no-stdout-log"  | tee /etc/turnserver.conf
 
 
-turnadmin -a -u $1 -p $2 -r $3
+turnadmin -a -u $usr -p $psw -r $realm
 
 turnserver
 
-echo "TURN server running. IP: "$externalIp" Username: $1, password: $2"
+echo "TURN server running. IP: "$externalIp" Username: $usr, password: $psw"
